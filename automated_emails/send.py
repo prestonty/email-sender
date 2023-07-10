7d8f991b70137814e4f180d1bede2caa1397e38d
Preston Tom-Ying <preston.tomying@gmail.com>
2023-07-10 16:48:03 -0400

    sends emails using csv file

--- automated_emails/send.py
+++ automated_emails/send.py
@@ -5,26 +5,44 @@ import smtplib
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+import csv
 
+# read from env file
+load_dotenv()
 sender = os.getenv('sender')
 password = os.getenv('pass')
-receiver = os.getenv('receiver')
+# receiver = os.getenv('receiver')
 
-subject = 'BIG WIN'
-body = """congragulations, you won the lottery"""
+# create the email body and message
 
-# object used to write email
-em = EmailMessage()
 
-em['From'] = sender
-em['To'] = receiver
-em['Subject'] = subject
-em.set_content(body)
+# read from csv
+with open('variables.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader) # skips the first row of the csv file (the header)
+    for row in csv_reader:
+        name = row[0]
+        email = row[1]
+        website = row[2]
+        status = row[3]
+        requested = row[4]
+        received = row[5]
+        money = row[6]
 
-context = ssl.create_default_context()
+        # object used to write email
+        em = EmailMessage()
 
-# smtp.gmail.com is an email server used to send the email
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(sender, password)
-    smtp.sendmail(sender, receiver, em.as_string())
+        subject = 'BIG WIN'
+        body = """congragulations, you won the lottery"""
+
+        em['From'] = sender
+        em['To'] = email
+        em['Subject'] = subject
+        em.set_content(body)
+
+        context = ssl.create_default_context()
+
+        # smtp.gmail.com is an email server used to send the email
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(sender, password)
+            smtp.sendmail(sender, email, em.as_string())
